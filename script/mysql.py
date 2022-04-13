f8354eea51cf1d19c275c22b43637d1ade976632
Trisna2828 <trisnaahidayat@gmail.com>
2022-04-13 20:59:52 +0700

    up

---
+++ script/mysql.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+
+
+class MySQL:
+  def __init__(self, config):
+    self.host = config['host']
+    self.port = config['port']
+    self.username = config['username']
+    self.password = config['password']
+    self.database = config['database']
+
+  def connect(self):
+    engine = create_engine('mysql+pymysql://{}:{}@{}:{}/{}'.format(self.username, self.password, self.host, self.port, self.database))
+    engine_conn = engine.connect()
+    print("Connect Engine MySQL")
+    return engine, engine_conn
