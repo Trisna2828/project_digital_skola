562e951bb4438d151ef93638e35ca310a261fe0f
Trisna2828 <trisnaahidayat@gmail.com>
2022-04-16 13:35:52 +0700

    data pipeline

--- sql/query.py
+++ sql/query.py
@@ -25,29 +25,29 @@ def create_table_fact(schema):
     date text,
     total bigint);
   CREATE TABLE IF NOT EXISTS {schema}.fact_province_monthly (
-      id SERIAL,
-      province_id text,
-      case_id int,
-      month text,
-      total bigint);
+    id SERIAL,
+    province_id text,
+    case_id int,
+    month text,
+    total bigint);
   CREATE TABLE IF NOT EXISTS {schema}.fact_province_yearly (
-      id SERIAL,
-      province_id text,
-      case_id int,
-      year text,
-      total bigint);
+    id SERIAL,
+    province_id text,
+    case_id int,
+    year text,
+    total bigint);
   CREATE TABLE IF NOT EXISTS {schema}.fact_district_monthly (
-      id SERIAL,
-      district_id text,
-      case_id int,
-      month text,
-      total bigint);
+    id SERIAL,
+    district_id text,
+    case_id int,
+    month text,
+    total bigint);
   CREATE TABLE IF NOT EXISTS {schema}.fact_district_yearly (
-      id SERIAL,
-      district_id text,
-      case_id int,
-      year text,
-      total bigint);
+    id SERIAL,
+    district_id text,
+    case_id int,
+    year text,
+    total bigint);
   """
 
   return query
