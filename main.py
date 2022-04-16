42b7012a124955971d5b73be9aa9f3cc92a8caaf
Trisna2828 <trisnaahidayat@gmail.com>
2022-04-16 12:11:47 +0700

    main functions

--- main.py
+++ main.py
@@ -23,18 +23,18 @@ def insert_raw_data():
   df = pd.DataFrame(data['data']['content'])
 
   df.columns = [x.lower() for x in df.columns.to_list()]
-  df.to_sql(name='aji_raw_covid', con=engine, if_exists="replace", index=False)
+  df.to_sql(name='trisna_raw_covid', con=engine, if_exists="replace", index=False)
   engine.dispose()
 
 def create_star_schema():
   postgre_auth = PostgreSQL(credential['postgresql_warehouse'])
   conn, cursor = postgre_auth.connect(conn_type='cursor')
 
-  query_dim = create_table_dim(schema='aji')
+  query_dim = create_table_dim(schema='project_3_trisna')
   cursor.execute(query_dim)
   conn.commit()
 
-  query_fact = create_table_fact(schema='aji')
+  query_fact = create_table_fact(schema='project_3_trisna')
   cursor.execute(query_fact)
   conn.commit()
 
@@ -43,5 +43,5 @@ def create_star_schema():
 
 
 if __name__ == '__main__':
-  # insert_raw_data()
-  create_star_schema()
+  insert_raw_data()
+  #create_star_schema()
